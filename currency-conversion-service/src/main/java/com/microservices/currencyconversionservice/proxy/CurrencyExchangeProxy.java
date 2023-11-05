d2306846794313aea108aa27b5177eb569c9f82e
Dilshat0402 <yusupzhanov.dilshat@gmail.com>
2023-11-05 15:24:16 +0600

    исправлен параметр eureka.client.serviceUrl.defaultZone

--- currency-conversion-service/src/main/java/com/microservices/currencyconversionservice/proxy/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/microservices/currencyconversionservice/proxy/CurrencyExchangeProxy.java
@@ -5,7 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange", url = "localhost:8000") //указываем название сервиса который хотим вызвать(который указан в app.prop)
+//@FeignClient(name = "currency-exchange", url = "localhost:8000") //указываем название сервиса который хотим вызвать(который указан в app.prop)
+@FeignClient(name = "currency-exchange")
 public interface CurrencyExchangeProxy {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyConversion getExchangeValue(
